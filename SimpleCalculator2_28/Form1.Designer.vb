c61e19d311d45fe50438edfa012d6277a3d750c0
vanthong <vanthongvision977@gmail.com>
2023-02-28 17:47:06 +0900

    プロジェクト ファイルを追加します。

---
+++ SimpleCalculator2_28/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Display = New System.Windows.Forms.Label()
+        Me.Num7 = New System.Windows.Forms.Button()
+        Me.Num8 = New System.Windows.Forms.Button()
+        Me.Num9 = New System.Windows.Forms.Button()
+        Me.Divide = New System.Windows.Forms.Button()
+        Me.Multiple = New System.Windows.Forms.Button()
+        Me.Num6 = New System.Windows.Forms.Button()
+        Me.Num5 = New System.Windows.Forms.Button()
+        Me.Num4 = New System.Windows.Forms.Button()
+        Me.Abstract = New System.Windows.Forms.Button()
+        Me.Num3 = New System.Windows.Forms.Button()
+        Me.Num2 = New System.Windows.Forms.Button()
+        Me.Num1 = New System.Windows.Forms.Button()
+        Me.Equal = New System.Windows.Forms.Button()
+        Me.Plus = New System.Windows.Forms.Button()
+        Me.Camma = New System.Windows.Forms.Button()
+        Me.Num0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Display
+        '
+        Me.Display.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Display.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Display.Font = New System.Drawing.Font("Century", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Display.ForeColor = System.Drawing.Color.White
+        Me.Display.Location = New System.Drawing.Point(12, 9)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(286, 60)
+        Me.Display.TabIndex = 0
+        Me.Display.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Num7
+        '
+        Me.Num7.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num7.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num7.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num7.Location = New System.Drawing.Point(12, 84)
+        Me.Num7.Name = "Num7"
+        Me.Num7.Size = New System.Drawing.Size(67, 37)
+        Me.Num7.TabIndex = 1
+        Me.Num7.Text = "7"
+        Me.Num7.UseVisualStyleBackColor = False
+        '
+        'Num8
+        '
+        Me.Num8.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num8.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num8.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num8.Location = New System.Drawing.Point(85, 84)
+        Me.Num8.Name = "Num8"
+        Me.Num8.Size = New System.Drawing.Size(67, 37)
+        Me.Num8.TabIndex = 2
+        Me.Num8.Text = "8"
+        Me.Num8.UseVisualStyleBackColor = False
+        '
+        'Num9
+        '
+        Me.Num9.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num9.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num9.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num9.Location = New System.Drawing.Point(158, 84)
+        Me.Num9.Name = "Num9"
+        Me.Num9.Size = New System.Drawing.Size(67, 37)
+        Me.Num9.TabIndex = 3
+        Me.Num9.Text = "9"
+        Me.Num9.UseVisualStyleBackColor = False
+        '
+        'Divide
+        '
+        Me.Divide.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Divide.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Divide.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Divide.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Divide.Location = New System.Drawing.Point(231, 84)
+        Me.Divide.Name = "Divide"
+        Me.Divide.Size = New System.Drawing.Size(67, 37)
+        Me.Divide.TabIndex = 4
+        Me.Divide.Text = "/"
+        Me.Divide.UseVisualStyleBackColor = False
+        '
+        'Multiple
+        '
+        Me.Multiple.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Multiple.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Multiple.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Multiple.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Multiple.Location = New System.Drawing.Point(231, 138)
+        Me.Multiple.Name = "Multiple"
+        Me.Multiple.Size = New System.Drawing.Size(67, 37)
+        Me.Multiple.TabIndex = 8
+        Me.Multiple.Text = "*"
+        Me.Multiple.UseVisualStyleBackColor = False
+        '
+        'Num6
+        '
+        Me.Num6.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num6.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num6.Location = New System.Drawing.Point(158, 138)
+        Me.Num6.Name = "Num6"
+        Me.Num6.Size = New System.Drawing.Size(67, 37)
+        Me.Num6.TabIndex = 7
+        Me.Num6.Text = "6"
+        Me.Num6.UseVisualStyleBackColor = False
+        '
+        'Num5
+        '
+        Me.Num5.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num5.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num5.Location = New System.Drawing.Point(85, 138)
+        Me.Num5.Name = "Num5"
+        Me.Num5.Size = New System.Drawing.Size(67, 37)
+        Me.Num5.TabIndex = 6
+        Me.Num5.Text = "5"
+        Me.Num5.UseVisualStyleBackColor = False
+        '
+        'Num4
+        '
+        Me.Num4.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num4.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num4.Location = New System.Drawing.Point(12, 138)
+        Me.Num4.Name = "Num4"
+        Me.Num4.Size = New System.Drawing.Size(67, 37)
+        Me.Num4.TabIndex = 5
+        Me.Num4.Text = "4"
+        Me.Num4.UseVisualStyleBackColor = False
+        '
+        'Abstract
+        '
+        Me.Abstract.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Abstract.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Abstract.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Abstract.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Abstract.Location = New System.Drawing.Point(231, 193)
+        Me.Abstract.Name = "Abstract"
+        Me.Abstract.Size = New System.Drawing.Size(67, 37)
+        Me.Abstract.TabIndex = 12
+        Me.Abstract.Text = "-"
+        Me.Abstract.UseVisualStyleBackColor = False
+        '
+        'Num3
+        '
+        Me.Num3.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num3.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num3.Location = New System.Drawing.Point(158, 193)
+        Me.Num3.Name = "Num3"
+        Me.Num3.Size = New System.Drawing.Size(67, 37)
+        Me.Num3.TabIndex = 11
+        Me.Num3.Text = "3"
+        Me.Num3.UseVisualStyleBackColor = False
+        '
+        'Num2
+        '
+        Me.Num2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num2.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num2.Location = New System.Drawing.Point(85, 193)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(67, 37)
+        Me.Num2.TabIndex = 10
+        Me.Num2.Text = "2"
+        Me.Num2.UseVisualStyleBackColor = False
+        '
+        'Num1
+        '
+        Me.Num1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num1.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num1.Location = New System.Drawing.Point(12, 193)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(67, 37)
+        Me.Num1.TabIndex = 9
+        Me.Num1.Text = "1"
+        Me.Num1.UseVisualStyleBackColor = False
+        '
+        'Equal
+        '
+        Me.Equal.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Equal.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Equal.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Equal.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Equal.Location = New System.Drawing.Point(231, 246)
+        Me.Equal.Name = "Equal"
+        Me.Equal.Size = New System.Drawing.Size(67, 37)
+        Me.Equal.TabIndex = 16
+        Me.Equal.Text = "="
+        Me.Equal.UseVisualStyleBackColor = False
+        '
+        'Plus
+        '
+        Me.Plus.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Plus.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Plus.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Plus.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Plus.Location = New System.Drawing.Point(158, 246)
+        Me.Plus.Name = "Plus"
+        Me.Plus.Size = New System.Drawing.Size(67, 37)
+        Me.Plus.TabIndex = 15
+        Me.Plus.Text = "+"
+        Me.Plus.UseVisualStyleBackColor = False
+        '
+        'Camma
+        '
+        Me.Camma.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Camma.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Camma.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Camma.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Camma.Location = New System.Drawing.Point(85, 246)
+        Me.Camma.Name = "Camma"
+        Me.Camma.Size = New System.Drawing.Size(67, 37)
+        Me.Camma.TabIndex = 14
+        Me.Camma.Text = ","
+        Me.Camma.UseVisualStyleBackColor = False
+        '
+        'Num0
+        '
+        Me.Num0.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Num0.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Num0.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Num0.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Num0.Location = New System.Drawing.Point(12, 246)
+        Me.Num0.Name = "Num0"
+        Me.Num0.Size = New System.Drawing.Size(67, 37)
+        Me.Num0.TabIndex = 13
+        Me.Num0.Text = "0"
+        Me.Num0.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(312, 301)
+        Me.Controls.Add(Me.Equal)
+        Me.Controls.Add(Me.Plus)
+        Me.Controls.Add(Me.Camma)
+        Me.Controls.Add(Me.Num0)
+        Me.Controls.Add(Me.Abstract)
+        Me.Controls.Add(Me.Num3)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.Num1)
+        Me.Controls.Add(Me.Multiple)
+        Me.Controls.Add(Me.Num6)
+        Me.Controls.Add(Me.Num5)
+        Me.Controls.Add(Me.Num4)
+        Me.Controls.Add(Me.Divide)
+        Me.Controls.Add(Me.Num9)
+        Me.Controls.Add(Me.Num8)
+        Me.Controls.Add(Me.Num7)
+        Me.Controls.Add(Me.Display)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Display As Label
+    Friend WithEvents Num7 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents Divide As Button
+    Friend WithEvents Multiple As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents Abstract As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents Num1 As Button
+    Friend WithEvents Equal As Button
+    Friend WithEvents Plus As Button
+    Friend WithEvents Camma As Button
+    Friend WithEvents Num0 As Button
+End Class
---
+++ SimpleCalculator2_28/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+    Private NumClicked As Boolean = False
+    Private ButtonClicked As Boolean = False
+
+    Private NumBuild As StringBuilder = New StringBuilder()
+
+    Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
+        NumClicked = True
+        Update(1)
+    End Sub
+
+    Private Sub Num2_Click(sender As Object, e As EventArgs) Handles Num2.Click
+        NumClicked = True
+        Update(2)
+    End Sub
+
+    Private Sub Num3_Click(sender As Object, e As EventArgs) Handles Num3.Click
+        NumClicked = True
+        Update(3)
+    End Sub
+    Private Sub Num4_Click(sender As Object, e As EventArgs) Handles Num4.Click
+        NumClicked = True
+        Update(4)
+    End Sub
+    Private Sub Num5_Click(sender As Object, e As EventArgs) Handles Num5.Click
+        NumClicked = True
+        Update(5)
+    End Sub
+    Private Sub Num6_Click(sender As Object, e As EventArgs) Handles Num6.Click
+        NumClicked = True
+        Update(6)
+    End Sub
+    Private Sub Num7_Click(sender As Object, e As EventArgs) Handles Num7.Click
+        NumClicked = True
+        Update(7)
+    End Sub
+    Private Sub Num8_Click(sender As Object, e As EventArgs) Handles Num8.Click
+        NumClicked = True
+        Update(8)
+    End Sub
+    Private Sub Num9_Click(sender As Object, e As EventArgs) Handles Num9.Click
+        NumClicked = True
+        Update(9)
+    End Sub
+    Private Sub Num0_Click(sender As Object, e As EventArgs) Handles Num0.Click
+        NumClicked = True
+        Update(0)
+    End Sub
+    Private Sub Comma_Click(sender As Object, e As EventArgs) Handles Comma.Click
+        NumClicked = True
+        Update(",")
+    End Sub
+
+
+    Private Sub Update(num As String)
+        NumBuild.Append(num)
+
+        Display.Text = NumBuild.ToString().TrimStart("0")
+
+
+
+    End Sub
+
+End Class
