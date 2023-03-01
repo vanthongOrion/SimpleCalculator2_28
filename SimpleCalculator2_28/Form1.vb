aa7fd51cfde5398e7fdae6bad8e9993f8fd70404
vanthong <vanthongvision977@gmail.com>
2023-03-01 10:33:08 +0900

    new commit

--- SimpleCalculator2_28/Form1.vb
+++ SimpleCalculator2_28/Form1.vb
@@ -8,12 +8,11 @@ Public Class Form1
     Private AbstractOn As Boolean = False
     Private MultipleOn As Boolean = False
     Private DivideOn As Boolean = False
-    Private EqualOn As Boolean = False
 
 
     Private InputNum1 As String = "0"
     Private InputNum2 As String = "0"
-    Private Result As Integer = 0
+    Private Result As Integer
 
     Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
         NumClicked = True
@@ -58,43 +57,59 @@ Public Class Form1
         Update(0)
     End Sub
     Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Plus.Click
-        If InputNum1 <> "0" Then
+        If Not (InputNum1 = "0" OrElse Result = 0) Then
             PlusOn = True
+            If InputNum2 <> 0 Then
+                Result = Calc(Integer.Parse(InputNum1), Integer.Parse(InputNum2))
+            End If
         End If
     End Sub
 
     Private Sub Abstract_Click(sender As Object, e As EventArgs) Handles Abstract.Click
-        If InputNum1 <> "0" Then
+        If Not (InputNum1 = "0" OrElse Result = 0) Then
             AbstractOn = True
         End If
     End Sub
     Private Sub Multiple_Click(sender As Object, e As EventArgs) Handles Multiple.Click
-        If InputNum1 <> "0" Then
+        If Not (InputNum1 = "0" OrElse Result = 0) Then
             MultipleOn = True
         End If
     End Sub
     Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
-        If InputNum1 <> "0" Then
+        If Not (InputNum1 = "0" OrElse Result = 0) Then
             DivideOn = True
         End If
     End Sub
 
     Private Sub Equal_Click(sender As Object, e As EventArgs) Handles Equal.Click
-        EqualOn = True
-
-        Result += Calc(Integer.Parse(InputNum1), Integer.Parse(InputNum2))
-        Show(Result)
-
+        Result = Calc(Integer.Parse(InputNum1), Integer.Parse(InputNum2))
     End Sub
 
     Private Sub Update(num As String)
-        If InputNum1 = "0" Then
-            InputNum1 += num
-            Show(InputNum1.TrimStart("0"))
-        ElseIf InputNum2 = "0" Then
-            InputNum2 += num
-            Show(InputNum2.TrimStart("0"))
+        Debug.Write("out" & vbCrLf)
+        If Result = Nothing Then
+            Debug.Write("in" & vbCrLf)
+            If PlusOn OrElse AbstractOn OrElse MultipleOn OrElse DivideOn Then
+                Debug.Write("First Block" & vbCrLf)
+                InputNum2 += num
+                Show(InputNum2.TrimStart("0"))
+            Else
+                Debug.Write("Second Block" & vbCrLf)
+                InputNum1 += num
+                Show(InputNum1.TrimStart("0"))
+            End If
+        Else
+            If PlusOn OrElse AbstractOn OrElse MultipleOn OrElse DivideOn Then
+                InputNum1 = Result.ToString()
+                Result = 0
+                InputNum2 += num
+                Show(InputNum2.TrimStart("0"))
+            Else
+                InputNum1 += num
+                Show(InputNum1.TrimStart("0"))
+            End If
         End If
+
     End Sub
 
     Private Sub Show(result As String)
@@ -102,31 +117,32 @@ Public Class Form1
     End Sub
 
     Private Function Calc(Number1 As Integer, Number2 As Integer) As Integer
+        Dim Result As Integer
         If PlusOn Then
+            Result = Number1 + Number2
             PlusOn = False
-            Return Number1 + Number2
         End If
 
         If AbstractOn Then
+            Result = Number1 - Number2
             AbstractOn = False
-            Return Number1 - Number2
         End If
 
         If MultipleOn Then
+            Result = Number1 * Number2
             MultipleOn = False
-            Return Number1 * Number2
         End If
 
         If DivideOn Then
+            Result = Number1 / Number2
             DivideOn = False
-            Return Number1 / Number2
         End If
-        Return 0
+
+        InputNum1 = ""
+        InputNum2 = ""
+        Show(Result)
+        Return Result
     End Function
-
-    Private Sub Main()
-
-    End Sub
 
 
 
