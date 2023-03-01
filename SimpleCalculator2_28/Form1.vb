72a11d582b7af0c04c711ce1fbdc25d5e8814fd9
nguyen van thong <nguyen van thong@DESKTOP-CE3J381>
2023-03-01 09:27:03 +0900

    hello

--- SimpleCalculator2_28/Form1.vb
+++ SimpleCalculator2_28/Form1.vb
@@ -4,8 +4,16 @@ Public Class Form1
 
     Private NumClicked As Boolean = False
     Private ButtonClicked As Boolean = False
+    Private PlusOn As Boolean = False
+    Private AbstractOn As Boolean = False
+    Private MultipleOn As Boolean = False
+    Private DivideOn As Boolean = False
+    Private EqualOn As Boolean = False
 
-    Private NumBuild As StringBuilder = New StringBuilder()
+
+    Private InputNum1 As String = "0"
+    Private InputNum2 As String = "0"
+    Private Result As Integer = 0
 
     Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
         NumClicked = True
@@ -49,19 +57,78 @@ Public Class Form1
         NumClicked = True
         Update(0)
     End Sub
-    Private Sub Comma_Click(sender As Object, e As EventArgs) Handles Comma.Click
-        NumClicked = True
-        Update(",")
+    Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Plus.Click
+        If InputNum1 <> "0" Then
+            PlusOn = True
+        End If
     End Sub
 
+    Private Sub Abstract_Click(sender As Object, e As EventArgs) Handles Abstract.Click
+        If InputNum1 <> "0" Then
+            AbstractOn = True
+        End If
+    End Sub
+    Private Sub Multiple_Click(sender As Object, e As EventArgs) Handles Multiple.Click
+        If InputNum1 <> "0" Then
+            MultipleOn = True
+        End If
+    End Sub
+    Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
+        If InputNum1 <> "0" Then
+            DivideOn = True
+        End If
+    End Sub
+
+    Private Sub Equal_Click(sender As Object, e As EventArgs) Handles Equal.Click
+        EqualOn = True
+
+        Result += Calc(Integer.Parse(InputNum1), Integer.Parse(InputNum2))
+        Show(Result)
+
+    End Sub
 
     Private Sub Update(num As String)
-        NumBuild.Append(num)
+        If InputNum1 = "0" Then
+            InputNum1 += num
+            Show(InputNum1.TrimStart("0"))
+        ElseIf InputNum2 = "0" Then
+            InputNum2 += num
+            Show(InputNum2.TrimStart("0"))
+        End If
+    End Sub
 
-        Display.Text = NumBuild.ToString().TrimStart("0")
+    Private Sub Show(result As String)
+        Display.Text = result
+    End Sub
 
+    Private Function Calc(Number1 As Integer, Number2 As Integer) As Integer
+        If PlusOn Then
+            PlusOn = False
+            Return Number1 + Number2
+        End If
 
+        If AbstractOn Then
+            AbstractOn = False
+            Return Number1 - Number2
+        End If
+
+        If MultipleOn Then
+            MultipleOn = False
+            Return Number1 * Number2
+        End If
+
+        If DivideOn Then
+            DivideOn = False
+            Return Number1 / Number2
+        End If
+        Return 0
+    End Function
+
+    Private Sub Main()
 
     End Sub
+
+
+
 
 End Class
